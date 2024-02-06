c18aa11c57132abefdd28bf1b041f806ce4ab774
Yatin Malhotra <yatin.a.malhotra@gmail.com>
2024-02-06 12:32:11 -0500

    Added NoteSearch functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react"
 import './App.css';
 import Dialog from "./Dialog";
 import Note from "./Note";
+import NoteSearch from "./NoteSearch";
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
   // -- Dialog props-- 
   const [dialogOpen, setDialogOpen] = useState(false)
   const [dialogNote, setDialogNote] = useState(null)
-
+  const [searchQuery, setSearchQuery] = useState("");
   
   // -- Database interaction functions --
   useEffect(() => {
@@ -129,6 +130,13 @@ function App() {
     }))
   }
 
+
+  const filteredNotes = searchQuery
+  ? notes.filter(note =>
+      note.title.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  : notes;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -136,12 +144,14 @@ function App() {
           <h1 style={AppStyle.title}>QuirkNotes</h1>
           <h4 style={AppStyle.text}>The best note-taking app ever </h4>
 
+          <NoteSearch searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+
           <div style={AppStyle.notesSection}>
             {loading ?
             <>Loading...</>
             : 
-            notes ?
-            notes.map((entry) => {
+            filteredNotes ?
+            filteredNotes.map((entry) => {
               return (
               <div key={entry._id}>
                 <Note
@@ -203,3 +213,4 @@ const AppStyle = {
     margin: "0px"
   }
 }
+
